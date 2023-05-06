f365f22788e8158c1c47473c703d8bb71280509d
Giovanni <gvn.gentile@gmail.com>
2023-05-05 23:06:29 -0300

    initial commit: quick and dirty

---
+++ README.md
@@ -0,0 +1,25 @@
+# Desafio do ciclo de palavras
+
+## Objetivo
+Este micro projeto tem como objetivo implementar o desafio do ciclo de palavras, comum em concursos, 
+provas e processos seletivos. Ele tem como proposta servir de "corretor", de modo que a pessoa consiga
+criar os próprios desafios, os resolva por conta própria e utilize o programa para uma correção do mesmo.
+
+Exemplo de um problema do ciclo de palavras: https://www.youtube.com/watch?v=75MdLJI2_g8
+
+## O que é o problema do Ciclo de Palavras (by ChatGPT)
+
+Esse problema consiste em encontrar a letra na posição _k_ em uma mesma palavra repetida _n_ vezes. 
+A solução para esse problema envolve a utilização do operador módulo para determinar em qual posição 
+da palavra repetida a letra se encontra. 
+
+Por exemplo, se a palavra tem tamanho _n_ e queremos encontrar a letra na posição _k_, a posição da letra na 
+palavra repetida é dada por _k_ % _n_. 
+
+## Como utilizar esse programa
+
+Clone o repositório na sua máquina, execute o método main e coloque a palavra que irá ciclar. Na sequência,
+digite a posição desejada. Após isso, resolva o exercício por conta própria e dê continuidade na execução
+do programa, para verificar se a sua resposta está igual ao que foi retornado.
+
+### Tente encontrar a resposta que retorna um easter egg 🖖
---
+++ src/com/giovicente/Cycles.java
@@ -0,0 +1,168 @@
+package com.giovicente;
+
+import java.util.Scanner;
+
+public class Cycles {
+
+    public static void main(String[] args) {
+
+        char continuationIndicator;
+        Scanner scanner;
+        String cycleWord;
+        final String SECRET_SPOCK_STRING = "LIVELONGANDPROSPER";
+        final int STAR_TREK_NUMBER_OF_EPISODES = 79;
+        int lastPosition;
+
+        do {
+            System.out.print("Digite a palavra que formará o ciclo: ");
+            scanner = new Scanner(System.in);
+            cycleWord = scanner.next().toUpperCase();
+            drawLine();
+
+            System.out.print("Digite a posição da última letra: ");
+            lastPosition = scanner.nextInt();
+            drawLine();
+
+            getAnswerIndicator(scanner);
+
+            int wordLenght = cycleWord.length();
+            System.out.println("O tamanho da palavra é: " + wordLenght);
+
+            int remainder = lastPosition % wordLenght;
+            System.out.println("O resto é igual a " + remainder);
+
+            // When the remainder is 0, the letter is in the last position
+            if (remainder == 0) remainder = wordLenght;
+
+            System.out.println("A letra da posição " + lastPosition + " da String "
+                    + cycleWord + " é " + cycleWord.charAt(remainder - 1));
+            drawLine();
+
+            continuationIndicator = getContinuationIndicator(scanner);
+
+        } while (continuationIndicator == 'S');
+
+        if (!(cycleWord.equals(SECRET_SPOCK_STRING) && lastPosition == STAR_TREK_NUMBER_OF_EPISODES)) {
+            System.out.println("Espero que tenha aproveitado o treino de lógica!");
+            drawLine();
+        } else {
+            drawSpock();
+        }
+
+        scanner.close();
+    }
+
+    private static void drawLine() {
+        System.out.println
+                ("* ----------------------------------------------------------- *");
+    }
+
+    private static void getAnswerIndicator(Scanner scanner) {
+        printAnswerIndicatorInputMessage();
+        char answerIndicator = scanner.next().charAt(0);
+        drawLine();
+
+        while (isInvalidAnswerIndicator(answerIndicator)) {
+            printInvalidCharacterMessage();
+            printAnswerIndicatorInputMessage();
+            answerIndicator = Character.toUpperCase(scanner.next().charAt(0));
+            drawLine();
+        }
+    }
+
+    private static void printAnswerIndicatorInputMessage() {
+        System.out.print("Deseja saber a resposta? [Aperte \"S\", e somente \"S\", para continuar]: ");
+    }
+
+    private static boolean isInvalidAnswerIndicator(char answerIndicator) {
+        return (answerIndicator != 'S');
+    }
+
+    private static char getContinuationIndicator(Scanner scanner) {
+        printContinuationIndicatorInputMessage();
+        char continuationIndicator = Character.toUpperCase(scanner.next().charAt(0));
+        drawLine();
+
+        while (isInvalidContinuationIndicator(continuationIndicator)) {
+            printInvalidCharacterMessage();
+            printContinuationIndicatorInputMessage();
+            continuationIndicator = Character.toUpperCase(scanner.next().charAt(0));
+            drawLine();
+        }
+
+        return continuationIndicator;
+    }
+
+    private static void printContinuationIndicatorInputMessage() {
+        System.out.print("Deseja tentar com outra palavra e posição? [S/N]: ");
+    }
+
+    private static boolean isInvalidContinuationIndicator(char continuationIndicator) {
+        return (continuationIndicator != 'S' && continuationIndicator != 'N');
+    }
+
+    private static void printInvalidCharacterMessage() {
+        System.out.println("Valor inválido. Por favor, digite um valor válido!!!");
+        drawLine();
+    }
+
+    private static void drawSpock() {
+        System.out.println(
+                        "                            .ss$$$$$$$$$$$$$$$ss.\n" +
+                        "                       .s$$$$$$$$$$$$$$$$$$$$$$$$$$ss.\n" +
+                        "                   .s$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$s\n" +
+                        "                 .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$.\n" +
+                        "               .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$s\n" +
+                        "             .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$s.\n" +
+                        "            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" +
+                        "          .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" +
+                        "          $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" +
+                        "        .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" +
+                        "        $$$$$$$$$$$$$$$$$$$$                                  $$\n" +
+                        "        $$$$$$$$$$$$$$$$$$     .\n" +
+                        "       s$$$$$$$$$$$$$$$$$     sssss.                           s\n" +
+                        "      ss $$$$$$$$$$$$$$$          ssss                      ss$s\n" +
+                        "     sss   $$$$$$$$$$$            ssss$$$$ss              $$$$s.\n" +
+                        "    sssss   $$$$$$$$   s.      .sss$$$$$$$$sss      sss$$$$$$s\n" +
+                        "    ss$ssss  $$$$$$$  sss.  .ss$$$$$$ss$ss     .         s$$$$s.\n" +
+                        "    ss$s   s  $$$$$$ ss.ss           ..        ss           .sss\n" +
+                        "     ss   s $s $$$$$ ss..s.                   ss\n" +
+                        "      s ss$$$$s $$$$ ssssss.                 .ss\n" +
+                        "    ss     s$$$s $$$ ssssssssss..           ssss.              .\n" +
+                        "    ss      $$  s $$ ss..ss$$$$sss        .sssss              ..\n" +
+                        "     s      s$$ss  $ ssss.sss$$$sss       .sssss.     ss..\n" +
+                        "      s.     $     $ .sss . .ss$sss        .  sssssss...   ss$\n" +
+                        "        ss   sssss$$ ..ss   ...sssss            .sss.       s\n" +
+                        "          ss    $$ $ .ss..s .sssssss\n" +
+                        "            ss$$  $$  .ssssss .sssss              ..       $\n" +
+                        "                $$$$  .ss$$ssss...s.s        ..sss$$ssss.\n" +
+                        "               $$.$$$  .ss$$sss.. sss.    .ssss$$s..s$ss. s\n" +
+                        "               ss ssss  .sssssss...sss.      .ssss$$ss..  .\n" +
+                        "               ss  ssss   .ssssssss.sss.        .sss.\n" +
+                        "               ss    .ssss    .sssssssss.               s\n" +
+                        "               ss      .sssss.  .sssssss.s .. ..ss$s. .s\n" +
+                        "              ss         sssssss.  .sssssssssssss$$s. .ss\n" +
+                        "            $ss           ssssssss.   ..sssssssssss  .ss.\n" +
+                        "          $$$$.            .sssssssss.     ssssss ssss.$s\n" +
+                        "      s$$$$$$$$s.             ..ssssssssss......ssss. s$$$s\n" +
+                        "    .s$$$$$$$$$$$s.              .ssssssssssssssss. s$$$$$ss.\n" +
+                        "       s$$$$$$$$$$$$$$ss..          .ssssssssss. .s$$$$$$$$s\n" +
+                        "         s$$$$$$$$$$$$$$$$$$$ssss...   ...ss.. s$$$$$$$$$s\n" +
+                        "           s$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$s\n" +
+                        "              .s$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$s\n" +
+                        "                    .s$$$$$$$$$$$$$$$$$$$$$$$$$$$$$s\n" +
+                        "                          .s$$$$$$$$$$$$$$$$$$$$s\n" +
+                        "                                ..s$$$$$$$$$$s"
+        );
+
+        System.out.println(
+                        "  _      _             _                                         _   _____                                        \n" +
+                        " | |    (_)           | |                                       | | |  __ \\                                      \n" +
+                        " | |     ___   _____  | |     ___  _ __   __ _    __ _ _ __   __| | | |__) | __ ___  ___ _ __   ___ _ __          \n" +
+                        " | |    | \\ \\ / / _ \\ | |    / _ \\| '_ \\ / _` |  / _` | '_ \\ / _` | |  ___/ '__/ _ \\/ __| '_ \\ / _ \\ '__|\n" +
+                        " | |____| |\\ V /  __/ | |___| (_) | | | | (_| | | (_| | | | | (_| | | |   | | | (_) \\__ \\ |_) |  __/ |         \n" +
+                        " |______|_| \\_/ \\___| |______\\___/|_| |_|\\__, |  \\__,_|_| |_|\\__,_| |_|   |_|  \\___/|___/ .__/ \\___|_|    \n" +
+                        "                                          __/ |                                         | |                       \n" +
+                        "                                         |___/                                          |_|                         ");
+    }
+}
