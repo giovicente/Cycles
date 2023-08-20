5be512c10ea3777ccf35ab6df776291444d7036e
Giovanni <gvn.gentile@gmail.com>
2023-08-19 22:27:36 -0300

    feat: update comments

--- src/com/giovicente/Cycles.java
+++ src/com/giovicente/Cycles.java
@@ -7,7 +7,7 @@ public class Cycles {
     /*
     * Unicode Characters
     * https://en.wikipedia.org/wiki/Latin-1_Supplement
-    *
+    * https://www.joelonsoftware.com/2003/10/08/the-absolute-minimum-every-software-developer-absolutely-positively-must-know-about-unicode-and-character-sets-no-excuses/
     * */
 
     public static void main(String[] args) {
